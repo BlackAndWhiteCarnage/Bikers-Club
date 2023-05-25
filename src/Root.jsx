a04c718d65edba4b5c25d85b19b147542d80a6eb
unknown <krzys.repsch@gmail.com>
2023-05-25 15:50:27 +0200

    feat: add Home section structure & new global css variables

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,8 +1,10 @@
 import { Navigation } from '@/components';
+import { Home } from '@/sections';
 
 const Root = () => (
 	<div>
 		<Navigation />
+		<Home />
 	</div>
 );
 
---
+++ src/sections/Home/Home.jsx
@@ -0,0 +1,8 @@
+/**
+ * External dependencies
+ */
+import classes from './Home.module.scss';
+
+const Home = () => <div className={classes.wrapper}>Home</div>;
+
+export default Home;
