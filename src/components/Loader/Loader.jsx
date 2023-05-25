611bffdf41649ccdc44aabff8ae5d4d500ce67d8
unknown <krzys.repsch@gmail.com>
2023-05-25 13:55:36 +0200

    feat: add Loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+/**
+ * External dependencies
+ */
+import classes from './Loader.module.scss';
+
+const Loader = () => (
+	<div className={classes.wrapper}>
+		<div className={classes.loader} />
+	</div>
+);
+
+export default Loader;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as Card } from './Card';
+export { default as Loader } from './Loader';
 export { default as Popover } from './Popover';
 export { default as Portal } from './Portal';
 export { default as Title } from './Title';
