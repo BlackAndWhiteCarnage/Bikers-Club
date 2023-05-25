93f0b71eda06d96035a5aa94455da40f9569bd1a
unknown <krzys.repsch@gmail.com>
2023-05-25 16:41:10 +0200

    feat: add hero video to Home section

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -2,7 +2,23 @@
  * External dependencies
  */
 import classes from './Home.module.scss';
+import ReactPlayer from 'react-player/lazy';
+import video from '@/videos/hero.mp4';
 
-const Home = () => <div className={classes.wrapper}>Home</div>;
+const Home = () => (
+	<>
+		<div className={classes.hero}>
+			<ReactPlayer
+				url={video}
+				muted
+				playing
+				loop
+				width="100%"
+				height="100%"
+			/>
+		</div>
+		<div className={classes.wrapper}></div>
+	</>
+);
 
 export default Home;
