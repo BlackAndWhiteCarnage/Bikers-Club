9ae4f74448a0a27a00dce64a6de3c71276dba572
BlackAndWhiteCarnage <krzys.repsch@gmail.com>
2023-05-28 11:40:50 +0200

    refactor: remove comment

--- src/components/ContextProvider/ContextProvider.jsx
+++ src/components/ContextProvider/ContextProvider.jsx
@@ -3,10 +3,6 @@
  */
 import { createContext, useState, useEffect } from 'react';
 
-/**
- * Internal dependencies
- */
-
 export const Context = createContext();
 
 const ContextProvider = ({ children }) => {
